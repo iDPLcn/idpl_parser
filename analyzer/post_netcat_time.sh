a86a24c8068cfd496696ce7b84fc64538f9487ef
kun22kun <kun22kun@126.com>
2015-05-21 16:34:37 +0800

    add shell of netcat to connect with db

---
+++ analyzer/post_netcat_time.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+USERNAME="idpl"
+PASSWORD="idpl"
+HOSTNAME="localhost:8000"
+API_URI="/condor/netcatdata/"
+
+SENDER=$1
+RECEIVER=$2
+TIME_START=$3
+TIME_END=$4
+CHECKSUM_EQUAL=$5
+DURATION=$6
+DATA_SIZE=$7
+BANDWIDTH=$8
+
+shift 8
+export  "$@"
+
+API_URL=http://$HOSTNAME$API_URI
+
+curl -u $USERNAME:$PASSWORD -H "Content-Type: application/json" -d "{\"source\": \"$SENDER\", \"destination\": \"$RECEIVER\", \"time_start\": $TIME_START, \"time_end\": $TIME_END, \"md5_equal\": $CHECKSUM_EQUAL, \"duration\": $DURATION, \"data_size\": $DATA_SIZE, \"bandwidth\": $BANDWIDTH}" $API_URL
